14db8f8fad721b5718bbd42f4caf33ae7c7f0592
unknown <sandeepshinde630@gmail.com>
2021-08-23 13:00:19 +0530

    feat:Added Deck of cards program

---
+++ DeckOfCards/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace DeckOfCards
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Deck Of Cards!");
+            Cards cards = new Cards();
+            cards.CardsCreator();
+        }
+    }
+}
